240ac07f51857e933dbcb01b3fec9dc21dce3e00
Felipe Rodrigues <carolinnerodrigues>
2019-07-01 01:18:23 -0300

    Adiciona controller e view de veiculos

---
+++ view/VeiculoView.php
@@ -0,0 +1,29 @@
+<?php
+
+   namespace view;
+
+   use view\base\BaseView;
+
+   class VeiculoView extends BaseView {
+
+        /*
+		* Método que renderiza elementos na tela
+		*/
+        public function render($controller, $optCommand) {
+
+            // Recupera parametros para serem usados na tela
+            $listObj = $this->param;
+
+            // Recupera o nome da classe
+            $actionName = $this->getName(__CLASS__);
+
+            // Renderiza a tela HTML
+            require_once "html/Veiculohrml.php";
+
+            // Renderiza Superclasse
+            parent::render($controller);
+        }
+
+        
+   }
+?>
---
+++ view/html/Veiculohtml.php
@@ -0,0 +1,46 @@
+<form action="" method="POST">
+
+<button type="submit" value="<?php echo $actionName.":"."BACK";?>" name="requestAction">Voltar</button>
+<br>
+<br>
+
+<?php 
+    if ($listObj != null && count($listObj) > 0) { 
+        foreach ($listObj as &$obj) { ?>
+
+            Modelo:<br>
+            <input type="text" name="modelo" value="<?php echo $obj->modelo != null ? $obj->modelo : null; ?>">
+            <br>
+            Ano:<br>
+            <input type="text" name="ano" value="<?php echo $obj->ano != null ? $obj->ano : null; ?>">
+            <br>
+            <br>
+
+<?php   } 
+    } else { ?>
+        
+            Modelo:<br>
+            <input type="text" name="modelo" value="">
+            <br>
+            Ano:<br>
+            <input type="text" name="ano" value="">
+            <br>
+            <br>
+
+<?php } ?>
+
+<?php if ($optCommand == "CREATE") { ?>
+
+    <button type="submit" value="<?php echo $actionName.":"."CREATE";?>" name="requestAction">Salvar</button>
+
+<?php } else if ($optCommand == "DELETE") { ?>
+
+    <button type="submit" value="<?php echo $actionName.":"."DELETE";?>" name="requestAction">Deletar</button>
+
+<?php } else if ($optCommand == "UPDATE") { ?>
+
+    <button type="submit" value="<?php echo $actionName.":"."UPDATE";?>" name="requestAction">Atualizar</button>
+
+<?php } ?>
+
+</form>
